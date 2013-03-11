059ce40960523f7f172f16537d7726af3dccbdc4
Nguyen Di Huynh <hdinguyen@gmail.com>
2013-03-11 21:44:48 +0700

    Initial Commit

---
+++ Smarthome/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Smarthome
+//
+//  Created by Nguyen Huynh on 3/11/13.
+//  Copyright (c) 2013 Nguyen Huynh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
